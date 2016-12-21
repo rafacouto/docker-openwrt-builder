3f82cbb957c79a57c9534c06374540b62c214e8f
Rafa Couto <caligari@treboada.net>
2016-12-21 06:55:36 +0100

    Log in detail while compiling.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# ToDo: show target options
+
+# ToDo: test target config
+
 cp "$WORKDIR/target-configs/$OPENWRT_TARGET.diff" \
 	"$WORKDIR/.config" && exit 1
 
 ./scripts/feeds update -a && exit 1
 ./scripts/feeds install -a && exit 1 
 
-PROCESSORS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
-ionice -c 3 nice -n19 make -j $((PROCESSORS - 1))
+ionice -c 3 nice -n19 make -j1 V=s
 
